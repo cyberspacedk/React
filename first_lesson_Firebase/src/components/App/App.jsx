6a229c177b7c34ed8745e445383a308d91f07df7
cyberspacedk <frontend.dk@gmail.com>
2019-04-22 10:33:21 +0300

    material ui

--- first_lesson_Firebase/src/components/App/App.jsx
+++ first_lesson_Firebase/src/components/App/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 // import Employee from "../Employee/Employee";
 // import Homepage from "../Home-page/Homepage"
-import Fire from '../Fire';
+import Fire from '../Fire'; 
+// pick utils
+import MomentUtils from '@date-io/moment';
+import DateFnsUtils from '@date-io/date-fns';
+import LuxonUtils from '@date-io/luxon';
+// import DateFnsUtils from "@date-io/date-fns"; // choose your lib
+import {
+  DatePicker,
+  TimePicker,
+  DateTimePicker
+} from "material-ui-pickers";
+
+
+import BasicDatePicker from '../DatePicker';
+
 import "./app.css";
 
 const App = () => (
@@ -9,8 +23,9 @@ const App = () => (
   <div className="App">
     {/* <Homepage />
     <Employee /> */}
-    <h2>Meet firebase</h2>
-    <Fire />
+    {/* <h2>Meet firebase</h2> */}
+    {/* <Fire /> */}
+    <BasicDatePicker />
   </div>
   
 );
