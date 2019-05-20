ba8cb5f2cfaa46c20fb576bfd459209e0a01dfd3
cyberspacedk <frontend.dk@gmail.com>
2019-05-20 16:53:32 +0300

    remove odd imports

--- colors_app/src/ColorPickerForm.js
+++ colors_app/src/ColorPickerForm.js
@@ -1,17 +1,7 @@
 import React, { Component } from "react";
-import PaletteFormNav from "./PaletteFormNav";
-import classNames from "classnames";
-import { withStyles } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
-import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
-import IconButton from "@material-ui/core/IconButton";
-import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import { ChromePicker } from "react-color";
 import Button from "@material-ui/core/Button";
-import DraggableColorList from "./DraggableColorList";
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
-import { arrayMove } from "react-sortable-hoc";
 
 class ColorPickerForm extends Component {
   state = {
