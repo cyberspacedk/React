31143327705056cbf857ce6d74f96212d19af827
cyberspacedk <frontend.dk@gmail.com>
2019-07-25 18:55:18 +0300

    GQL

--- level_1_react_apollo_graphql/src/App.js
+++ level_1_react_apollo_graphql/src/App.js
@@ -1,53 +1,30 @@
-import React, { Component } from 'react';
-import ApolloClient from 'apollo-boost';
-import {ApolloProvider, Query} from 'react-apollo';
-import gql from 'graphql-tag'; 
-
-
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import Post from "../src/posts/Post";
+import Posts from "../src/posts/Posts";
 
 // connecting out site to the GraphQl API
 const client = new ApolloClient({
-  // uri: 'https://api-uswest.graphcms.com/v1/cjjoov78z2j9t01gclcnhwlb9/master'
-  uri: 'https://api-euwest.graphcms.com/v1/cjwkd8ou80iq601fmmd4zeymg/master'
-
+  uri: "https://api-euwest.graphcms.com/v1/cjwkd8ou80iq601fmmd4zeymg/master"
 });
 
-// Write our query
-const POSTS_QUERY = gql`
-query allposts{
-  posts{
-    id
-    createdAt
-    title
-    body
-  }
-}`;
-
-// Running query outside of React
-// client.query({
-//   query: testQuery
-// })
-// .then(data=> console.log(data))
- 
-
-// Apollo Provider attachet the client to our React app
 class App extends Component {
-
   render() {
     return (
       <ApolloProvider client={client}>
-      <div className="App"> 
-        <p>TEST QUERY BELOW</p>
-         <Query query={POSTS_QUERY}>
-           {({loading, data:{posts}})=> {
-             if(loading) return "Loading..."; 
-             return posts.map(post=> (<h1 key={Math.random()*24235 | 0}>{post.title}</h1>))
-           }}
-         </Query>
-
-      </div>
+        <Router>
+          <div className="App">
+            <p>TEST QUERY BELOW</p>
+            <Switch>
+              <Route path="/post/:id" component={Post} />
+              <Route path="/" component={Posts} />
+            </Switch>
+          </div>
+        </Router>
       </ApolloProvider>
     );
   }
