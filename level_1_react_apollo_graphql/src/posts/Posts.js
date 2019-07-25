a83c4c390011cb9f11983e28788630e02b2ac058
cyberspacedk <frontend.dk@gmail.com>
2019-07-25 18:55:52 +0300

    gql

---
+++ level_1_react_apollo_graphql/src/posts/Posts.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import { Link } from "react-router-dom";
+
+const Posts = () => (
+  <Query query={POSTS_QUERY}>
+    {({ loading, data: { posts } }) => {
+      if (loading) return "Loading...";
+      return posts.map(post => (
+        <Link to={`posts/${post.id}`}>
+          <h1 key={(Math.random() * 24235) | 0}>{post.title}</h1>
+        </Link>
+      ));
+    }}
+  </Query>
+);
+
+export default Posts;
+
+// Write our query
+const POSTS_QUERY = gql`
+  query allposts {
+    posts {
+      id
+      createdAt
+      title
+      body
+    }
+  }
+`;
